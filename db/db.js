87c97367304e7096597939724ba190d10a48a72a
mc manuel ptel <primomanuel@hotmail.com>
2025-06-01 21:28:17 -0600

    as

--- db/db.js
+++ db/db.js
@@ -1,7 +1,8 @@
+const { Pool } = require('pg');
 const pool = new Pool({
-	user: 'tu_usuario', // ¿Es tu nombre de usuario correcto?
+	user: 'postgres', // ¿Es tu nombre de usuario correcto?
 	host: 'localhost', // ¿Es el servidor correcto?
-	database: 'mi_base_de_datos', // ¿El nombre de la base es correcto?
-	password: 'tu_contraseña', // ¿La contraseña es correcta?
+	database: 'PTECOL', // ¿El nombre de la base es correcto?
+	password: '', // ¿La contraseña es correcta?
 	port: 5432, // El puerto por defecto de PostgreSQL
 });
